64f32e3209655f42304c1fb92af373c46794eb0f
Gopika Chauhan <156003501+Cephei18@users.noreply.github.com>
2026-04-01 17:02:37 +0530

    Hosted Menifest by farcaster

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Keep defaults so Vercel picks up .next output automatically.
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/019d48ca-6b23-5935-c634-3230c9f0c41d",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
